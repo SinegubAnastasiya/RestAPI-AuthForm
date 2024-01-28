acba9ecdcbb59f6873a61345b8c57efa3a652171
SinegubAnastasiya <fedukevich98@mail.ru>
2024-01-28 16:00:39 +0300

    added dotenv

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: '127.0.0.1',
   port: '5432',
-  password: 'kitten1998',
+  password: process.env.PASSWORD,
   user: 'postgres',
   database: 'authForm',
 });
