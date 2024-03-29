5e2a94c322bd99fc103d9934f640516114d290b4
SinegubAnastasiya <fedukevich98@mail.ru>
2024-03-29 20:58:40 +0300

    fixed

--- client/reg.js
+++ client/reg.js
@@ -6,6 +6,11 @@ btn.addEventListener('click', async () => {
     const phone = document.querySelector('.phone').value
     const pwd = document.querySelector('.pwd').value
 
+    const usernameP = document.querySelector('.username_p')
+    const emailP = document.querySelector('.email_p')
+    const phoneP = document.querySelector('.phone_p')
+    const idP = document.querySelector('.id_p')
+
     const response = await fetch('http://localhost:3000/user/reg', {
         method: 'POST',
         headers: {
@@ -16,4 +21,9 @@ btn.addEventListener('click', async () => {
 
     const result = await response.json()
     console.log(result);
+
+    usernameP.innerHTML = result[0].username
+    emailP.innerHTML = result[0].email
+    phoneP.innerHTML = result[0].phone
+    idP.innerHTML = result[0].id
 })
---
+++ client/update.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', async () => {
+    const username = document.querySelector('.username').value
+    const email = document.querySelector('.email').value
+    const phone = document.querySelector('.phone').value
+    const pwd = document.querySelector('.pwd').value
+
+    const response = await fetch ()
+})
